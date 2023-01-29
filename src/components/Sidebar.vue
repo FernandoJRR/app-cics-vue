cecf749b25714a5aad72c8bf081b5a80aeb0c690
FernandoJRR <fercitotron@gmail.com>
2023-01-28 22:27:30 -0600

    Agregar componente Sidebar, eliminar archivos de bienvenida de Vue3

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        class="bg-deep-green"
+        theme="dark"
+        permanent
+      >
+        <v-list color="gray">
+          <v-list-item prepend-icon="fa-calendar" title="Home" to='/'/>
+          <v-list-item prepend-icon="fa-calendar" title="Acerca de" to='about'/>
+        </v-list>
+
+      </v-navigation-drawer>
+    </v-layout>
+  </v-card>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import HelloWorld from "../components/HelloWorld.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <HelloWorld msg="App del CICS (TM)" />
   </main>
 </template>
