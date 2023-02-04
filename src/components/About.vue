f61fe370c756e2de7c1d4ef64a434dbb83f7ccae
Roberto Bautista <robertobau45@gmail.com>
2023-02-04 03:42:38 -0600

    Updated about page content

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,26 +1,30 @@
 <template>
   <v-container class="mt-4">
     <p>
-      La Aplicacion para Estudiantes de Ingenieria del CUNOC, nacio como un proyecto hecho por y para estudiantes con el fin de facilitarles 
-      el acceso a informacion de la Division para ayudarles en su viaje a lo largo de la carrera.
-      Esta aplicacion fue desarrollada con el afan de que los estudiantes tengamos mejores fuentes de informacion y sentar un 
+      La Aplicación para Estudiantes de Ingenieria del CUNOC, nació como un proyecto hecho por y para estudiantes con el fin de facilitarles 
+      el acceso a información de la División para ayudarles en su viaje a lo largo de la carrera.
+      Esta aplicación fue desarrollada con el afán de que los estudiantes tengamos mejores fuentes de información y sentar un 
       precedente de que es posible tener mejores sistemas informaticos en el CUNOC.
       <br>
       <br>
-      Desde la comunidad de la carrera de Ciencias y Sistemas y especialmente desde el CICS esperamos que disfruten y den buen uso a esta herramienta &hearts;
+      Desde la comunidad de la carrera de Ciencias y Sistemas y especialmente desde el Comité de Ingeniería en Ciencias y Sistemas esperamos que disfruten y den buen uso a esta herramienta &hearts;
     </p>
     <h2>
       Desarrollo
     </h2>
     <v-divider thickness="3" />
     <p>
-      La aplicacion fue desarrollada pensando primero en su utilidad en dispositivos moviles debido al gran uso de este por los estudiantes para obtener informacion.
-      Debido a las necesidades de la aplicacion esta fue realizada completamente utilizando el framework Vue 3 y Vite.
-      Debido a la falta de presupuesto decidimos utilizar Github Pages para hostear la pagina y que esta fuera accesible a cualquier persona que quisiera acceder.
+      La aplicación fue desarrollada pensando primero en su utilidad en dispositivos móviles debido al gran uso de este por los estudiantes para obtener información.
+      Debido a las necesidades de la aplicación esta fue realizada completamente utilizando el framework web <strong>Vue.js 3</strong>, acompañado de <strong>Vite</strong> y el framework de interfaces <strong>Vuetify</strong>.
+      Debido a la falta de presupuesto decidimos utilizar <strong> GitHub Pages </strong> para hostear la pagina y que esta fuera accesible a cualquier persona que quisiera acceder.
       <br>
       <br>
-      En la comunidad de Ciencias y Sistemas del CUNOC y desde el CICS apoyamos el conocimiento libre y el software de codigo abierto, por lo que todo el codigo
-      utilizado en la aplicacion esta disponible en Github -insertar repositorio-.
+      En la comunidad de Ciencias y Sistemas del CUNOC y desde el CICS apoyamos el conocimiento libre y el software de código abierto, por lo que todo el código
+      utilizado en la aplicación esta disponible en el  
+      <a
+        href="https://github.com/FernandoJRR/app-cics-vue"
+        target="_blank"
+      >repositorio de GitHub</a>.
     </p>
 
     <h2>
@@ -28,52 +32,52 @@
     </h2>
     <v-divider thickness="3" />
     <p>
-      Esta aplicacion fue desarrollada por 2 estudiantes del CICS en su tiempo libre por lo que aun es un trabajo sujeto a mejoras.
-      Nuestra idea al crear esta pagina es que pueda ser ampliada y mejorada a lo largo del tiempo por otros estudiantes en el futuro con
-      intensiones de ayudar a otros, como nosotros al crear esta pagina, por ello cualquier sugerencia para mejorar la pagina es bienvenida.
+      Esta aplicación fue desarrollada por 2 estudiantes del CICS en su tiempo libre por lo que aún es un trabajo sujeto a mejoras.
+      Nuestra idea al crear esta página es que pueda ser ampliada y mejorada a lo largo del tiempo por otros estudiantes en el futuro con
+      intenciones de ayudar a otros, como nosotros al crear esta página, por ello cualquier sugerencia para mejorar la página es bienvenida.
       <br>
       <br>
-      La siguiente seccion describe las formas en las que pueden aportar para el desarrollo y mantenimiento de la aplicacion incluso si no tienen conocimientos de computacion.
+      La siguiente sección describe las formas en las que pueden aportar para el desarrollo y mantenimiento de la aplicación incluso si no tienen conocimientos de computacion.
     </p>
     <h2>
-      Como contribuir
+      ¿Como contribuir?
     </h2>
     <v-divider thickness="3" />
     <p>
-      Hay dos formas en las que puedes contribuir a la aplicacion, la primera forma es contribuir a su mantenimiento y expansion, para esto hemos creado el apartado Herramientas con el fin
-      de que interesados puedan desarrollar y proponer sus propios snippets hechos en Vue 3 para ampliar la funcionalidad de la aplicacion con herramientas que ellos consideren importantes.
+      Hay dos formas en las que puedes contribuir a la aplicación, la primera forma es contribuir a su mantenimiento y expansión, para esto hemos creado el apartado <strong>Herramientas</strong> con el fin
+      de que interesados puedan desarrollar y proponer sus propios snippets hechos en Vue 3 para ampliar la funcionalidad de la aplicación con herramientas que ellos consideren importantes.
       Para esto puedes contactar al CICS, el correo estara en el apartado del mismo nombre.
 
-      La siguiente forma de contribuir al desarrollo de la aplicacion es la de reportar los posibles errores encontrados al utilizar la pagina,
-      hay dos formas de hacer esto, la primera es abrir un Issue en el repositorio de Github para que los que mantenemos la pagina podamos solucionarlo,
-      y la segunda forma es enviar un correo al CICS con el asunto "Resporte de Error - App CICS".
+      La siguiente forma de contribuir al desarrollo de la aplicación es la de reportar los posibles errores encontrados al utilizar la página,
+      hay dos formas de hacer esto, la primera es abrir un <strong>Issue</strong> en el repositorio de GitHub para que quienes mantenemos la página podamos solucionarlo,
+      y la segunda forma es enviar un correo al CICS con el asunto "Reporte de Error - App CICS".
       <br>
       <br>
-      Debido a la complejidad de la pagina les pedimos encarecidamente que los reportes de los errores sean lo mas especificos posibles para que nosotros podamos resolverlos de la 
-      manera mas rapida y eficiente posible, por ello mismo hemos listado un conjunto de aspectos que sugerimos que el reporte del error tenga.
+      Debido a la complejidad de la pagina les pedimos encarecidamente que los reportes de los errores sean lo mas específicos posibles para que nosotros podamos resolverlos de la 
+      manera mas rápida y eficiente posible, por ello mismo hemos listado un conjunto de aspectos que sugerimos que el reporte del error tenga.
       <br>
       <br>
-      - Una descripcion clara y detallada de como es el error.
+      ● Una descripción clara y detallada de como es el error.
       <br>
-      - Detalles tecnicos del dispositivo, si se utilizo en movil o computadora, nombre del dispositivo, sistema operativo, nombre del navegador, etc
+      ● Detalles técnicos del dispositivo, si se utilizo en móvil o computadora, nombre del dispositivo, sistema operativo, nombre del navegador, etc.
       <br>
-      - Como reproducir el error
+      ● ¿Como reproducir el error?
       <br>
       &emsp;Por ejemplo:
       <br>
-      &emsp;&emsp;1. Abrir la seccion Pensums
+      &emsp;&emsp;1. Abrir la seccion "Pensums"
       <br>
       &emsp;&emsp;2. Ingresar al Pensum de Ingenieria Civil
       <br>
-      &emsp;&emsp;3. Pulsar X boton
+      &emsp;&emsp;3. Pulsar "X" boton
       <br>
       &emsp;&emsp;4. Pulsar Enter
       <br>
-      - El comportamiento que se esperaba que ocurriera
+      ● El comportamiento que se esperaba que ocurriera
       <br>
-      - Screenshots del error
+      ● Capturas de pantalla del error
       <br>
-      - Contexto adicional
+      ● Contexto adicional
     </p>
   </v-container>
 </template>
