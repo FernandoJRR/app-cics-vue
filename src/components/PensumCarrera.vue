51e7d633679461515356a0efc4205cc70d02aaf6
FernandoJRR <fercitotron@gmail.com>
2023-02-16 17:01:32 -0600

    Agregar componente de tarjeta prototipo de semestre, agregar carreras y tarjetas a PensumsView

---
+++ src/components/PensumCarrera.vue
@@ -0,0 +1,122 @@
+<template>
+  <h1 align="center">{{titulo}}</h1>
+  <v-container>
+    <v-btn
+      style="margin-bottom: 3%;"
+      prepend-icon="mdi-arrow-left"
+      @click="regresarATesario()"
+    >Regresar</v-btn>
+    <v-row justify="center">
+      <v-col
+        v-for="semestre in semestres"
+        cols="12"
+        xs="12"
+        sm="12"
+        lg="4"
+      >
+      <PensumCard :color="colorCarrera" :semestre="semestre"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import Banner from "../components/Banner.vue"
+import PensumCard from "../components/cards/PensumCard.vue"
+import { useRoute } from "vue-router"
+import router from "@/router"
+export default {
+  components: {
+    Banner,
+    PensumCard
+  },
+  emits: ['currentPage'],
+  mounted() {
+    window.scrollTo(0,0)
+    switch (useRoute().path) {
+      case "/pensums/civil":
+        this.titulo = "Civil"
+        this.carreraActual = "civil"
+        this.colorCarrera = "bg-red-darken-1"
+        break;
+      case "/pensums/ciencias-sistemas":
+        this.titulo = "Ciencias y Sistemas"
+        this.carreraActual = "sistemas",
+        this.colorCarrera = "bg-orange-accent-4"
+        break;
+      case "/pensums/mecanica":
+        this.titulo = "Mecanica"
+        this.carreraActual = "mecanica"
+        this.colorCarrera = "bg-green-darken-1"
+        break;
+      case "/pensums/industrial":
+        this.titulo = "Industrial"
+        this.carreraActual = "industrial"
+        this.colorCarrera = "bg-light-green-darken-2"
+        break;
+      case "/pensums/mecanica-industrial":
+        this.titulo = "Mecanica Industrial"
+        this.carreraActual = "mecanica-industrial"
+        this.colorCarrera = "bg-teal-darken-2"
+        break;
+      default:
+        break;
+    }
+  },
+  data() {
+    return {
+      bannerImg: new URL("@/assets/img/tesario.jpg", import.meta.url).href,
+      titulo: '',
+      carreraActual: '',
+      colorCarrera: '',
+      semestres : [
+        {
+          nombre: "Primer",
+          numero: "1"
+        },
+        {
+          nombre: "Segundo",
+          numero: "2"
+        },
+        {
+          nombre: "Tercer",
+          numero: "3"
+        },
+        {
+          nombre: "Cuarto",
+          numero: "4"
+        },
+        {
+          nombre: "Quinto",
+          numero: "5"
+        },
+        {
+          nombre: "Sexto",
+          numero: "6"
+        },
+        {
+          nombre: "Septimo",
+          numero: "7"
+        },
+        {
+          nombre: "Octavo",
+          numero: "8"
+        },
+        {
+          nombre: "Noveno",
+          numero: "9"
+        },
+        {
+          nombre: "Decimo",
+          numero: "10"
+        },
+      ]
+    }
+  },
+  methods: {
+    regresarATesario() {
+      router.push("/pensums")
+    }
+  },
+}
+</script>
+
---
+++ src/components/cards/PensumCard.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="374"
+  >
+    <v-card-title 
+      :class="color"
+    >
+      <v-row no-gutters>
+        <v-col>
+          <v-icon
+            :icon="'mdi-numeric-'+semestre.numero"
+            size="80"
+          ></v-icon>
+        </v-col>
+        <v-col class="pa-2">
+          <h2 style="text-align: right;">{{semestre.nombre}} <br> Semestre</h2>
+        </v-col>
+      </v-row>
+    </v-card-title>
+
+    <v-divider class="mx-4 mb-1"></v-divider>
+
+    <v-card-item>
+      <v-card-title>Cafe Badilico</v-card-title>
+
+      <v-card-subtitle>
+        <span class="me-1">Local Favorite</span>
+
+        <v-icon
+          color="error"
+          icon="mdi-fire-circle"
+          size="small"
+        ></v-icon>
+      </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :model-value="4.5"
+          color="amber"
+          density="compact"
+          half-increments
+          readonly
+          size="small"
+        ></v-rating>
+
+        <div class="text-grey ms-4">
+          4.5 (413)
+        </div>
+      </v-row>
+
+      <div class="my-4 text-subtitle-1">
+        $ • Italian, Cafe
+      </div>
+
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-divider class="mx-4 mb-1"></v-divider>
+
+    <v-card-title>Seleccionar</v-card-title>
+
+    <div class="px-4">
+      <v-chip-group :v-model="1">
+        <v-chip>Obligatorios</v-chip>
+
+        <v-chip>Opcionales</v-chip>
+
+        <v-chip>Todos</v-chip>
+      </v-chip-group>
+    </div>
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple-lighten-2"
+        variant="text"
+      >
+        Accion
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    color: {
+      type: String,
+      required: true
+    },
+    semestre: {
+      type: Object,
+      required: true
+    },
+  }
+}
+</script>
